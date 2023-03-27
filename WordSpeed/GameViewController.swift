be26253d2720549828b6b1db3906e93133f51e2f
lizahjortling <lizhj90@outlook.com>
2023-03-27 09:39:50 +0200

    fix the gametimer

--- WordSpeed/GameViewController.swift
+++ WordSpeed/GameViewController.swift
@@ -8,11 +8,23 @@ class GameViewController: UIViewController {
         timer = Timer.scheduledTimer(withTimeInterval: 1 , repeats: true, block: {_ in self.TimerCounter()})
         CurrentWordLabel.text = GameWordList.choosenList.randomElement()
     }
-    
+    var totalGameCounter = 60
     var counter = 10
     var timer: Timer?
     var score = 0
     
+    @IBAction func ResetGameButton(_ sender: Any) {
+        resetTimeAndWord()
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(withTimeInterval: 1 , repeats: true, block: {_ in self.TimerCounter()})
+        resetScore()
+        
+       
+       
+    }
+   
+    @IBOutlet weak var GameCounterLabel: UILabel!
+   
     @IBOutlet weak var WWTExtField: UITextField!
     
     @IBOutlet weak var GameTimer: UILabel!
@@ -22,6 +34,7 @@ class GameViewController: UIViewController {
     @IBOutlet weak var ScoreLabel: UILabel!
     
     @IBAction func WriteWordTextField(_ sender: UITextField) {
+       
         
         checkWrongOrRight()
         WWTExtField.text = ""
@@ -30,6 +43,15 @@ class GameViewController: UIViewController {
     @IBOutlet weak var CurrentWordLabel: UILabel!
     
     func TimerCounter () {
+    
+        totalGameCounter -= 1
+        GameCounterLabel.text = totalGameCounter.formatted()
+        
+        if totalGameCounter == 0 {
+            showToast(message: "End Of Game ! ", font: .systemFont(ofSize: 17.0))
+            timer?.invalidate()
+        }
+        
         
         counter -= 1
         GameTimer.text = counter.formatted()
@@ -42,16 +64,27 @@ class GameViewController: UIViewController {
                 checkWrongOrRight()
                 resetTimeAndWord()
                 showToast(message: "Times Up! ", font: .systemFont(ofSize: 17.0))
+                
+                
             }
-            }
+        }
+        if counter <= 5 {
+            GameTimer.textColor = .red
+        }else{
+            GameTimer.textColor = .white
+        }
     }
     func resetTimeAndWord () {
         GameTimer.text = "10"
+        GameTimer.textColor = .white
         CurrentWordLabel.text = GameWordList.choosenList.randomElement()
         counter = 10
         WWTExtField.text = ""
     }
     func checkWrongOrRight() {
+        if totalGameCounter == 0 {
+            return 
+        }
         if (CurrentWordLabel.text == WWTExtField.text) {
             CurrentWordLabel.text = GameWordList.choosenList.randomElement()
             score += 1
@@ -65,4 +98,12 @@ class GameViewController: UIViewController {
         }
         
     }
+    func resetScore () {
+       score = 0
+        ScoreLabel.text = score.formatted()
+    }
+    func countdownTimerForTheWholeGame () {
+       
+    }
 }
+
